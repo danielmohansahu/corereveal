df0d5ef337f7ef536df2241b4530fbace5cffbed
Daniel M. Sahu <danielmohansahu@gmail.com>
2023-04-16 08:46:05 -0400

    Hello World example of a working script; slight restructuring.
    Signed-off-by: Daniel M. Sahu <danielmohansahu@gmail.com>

---
+++ CoreReveal/corereveal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# CoreReveal - Ghidrathon based Ghidra script that executes and analyzes a Qiling emulation.
+# @category: CoreReveal
+
+# STL
+import sys
+
+# Qiling
+from qiling import Qiling
+
+if __name__ == "__main__":
+    popup("Hello, world!")
--- build_and_run.sh
+++ build_and_run.sh
@@ -7,7 +7,7 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # build and tag docker image
 IMAGE=ghidra-development:latest
-docker build -t $IMAGE $SCRIPTPATH/docker/
+docker build -t $IMAGE $SCRIPTPATH/
 
 # drop into a container
-rocker --x11 --volume $SCRIPTPATH:/workspace -- $IMAGE bash
+rocker --x11 --volume $SCRIPTPATH:/workspace -- $IMAGE byobu
