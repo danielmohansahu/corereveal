0be2469c599cc3dce38aa5be466ee9c18cf7ed54
Daniel M. Sahu <danielmohansahu@gmail.com>
2023-04-23 08:36:40 -0400

    Initial commit of partially implemented Mock class for Frontend development.
    Signed-off-by: Daniel M. Sahu <danielmohansahu@gmail.com>

---
+++ src/corereveal/mock_qiling_interface.py
@@ -0,0 +1,60 @@
+""" Mock implementation of QilingInterface.
+
+This class is a dummy implementation of the QilingInterface
+class for testing and development.
+"""
+
+# STL
+import time
+import random
+from typing import Callable
+
+# CoreReveal
+from .qiling_interface import QilingInterface, EmulationResults
+
+class MockQilingInterface:
+    """ Mock QilingInterface class, for testing and development. """
+    def __init__(self, program, metadata, blocks, static_variables):
+        """ Mock initialization takes the same arguments as the core class
+            as well as some Ghidra-supplied program info.
+        """
+        self.program  = program
+        self.metadata = metadata
+        self.blocks = blocks
+        self.static_variables = static_variables
+
+    def emulate(self, args: str, stdin_cb: Callable, stdout_cb: Callable) -> EmulationResults:
+        """ Simulate emulation. """
+
+        # determine duration
+        duration = random.random() * 10
+        
+        # 50/50 chance of prompting for user input right off the bat
+        if random.random() > 0.5:
+            stdin_cb("How are you feeling?")
+        stdout_cb("I understand.")
+        
+        # loop until desired time is up
+        st = time.time()
+        while (time.time() - st < duration):
+            # maybe we need more user input
+            if random.random() > 0.9:
+                stdin_cb("Please provide additional input")
+            
+            # it's more likely that we just spam 
+            if random.random() > 0.5:
+                stdout_cb("SPAM!")
+            
+            # sleep
+            time.sleep(random.random())
+
+        # there's a small chance emulation failed
+        if random.random() < 0.2:
+            stdout_cb("SEGFAULT")
+            return None
+        
+        # otherwise, populate random results
+        results = EmulationResults()
+
+        # @TODO populate
+        return results
