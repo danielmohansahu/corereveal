60515ceb23cd968beef5e338f5f38495be02c006
Daniel M. Sahu <danielmohansahu@gmail.com>
2023-04-23 07:52:57 -0400

    Fleshing out core Front-End operations.
    Signed-off-by: Daniel M. Sahu <danielmohansahu@gmail.com>

--- scripts/CoreReveal.py
+++ scripts/CoreReveal.py
@@ -1,3 +1,4 @@
+# CoreReveal - Qiling based program flow emulation and visualization.
 """CoreReveal - Ghidrathon based Ghidra script that executes and analyzes a Qiling emulation.
 
 CoreReveal performs emulation via Qiling and analyzes the program flow.
@@ -14,11 +15,58 @@ them all run `dir(builtins)`.
 # STL
 import sys
 
-# CoreReveal
-from corereveal.qiling_interface import QilingInterface
-
 # sanity check we're using Python3 (via Ghidrathon)
 assert sys.version_info > (3,0), "Incorrect Python version; do you have Ghidrathon installed?"
 
+# Ghidra
+from ghidra.program.flatapi import FlatProgramAPI
+
+# CoreReveal
+from corereveal.qiling_interface import QilingInterface
+
+def annotate_bss(api, variables: dict):
+    """ Add annotations to the current program detailing BSS values. """
+    # @TODO!
+
+def annotate_posix_calls(api, posix_calls: dict):
+    """ Add annotations to the current program detailing POSIX call arguments. """
+    # @TODO!
+
+def color_function_graph(api, blocks: list):
+    """ Highlight the blocks encountered and display the Function Graph. """
+    # @TODO!
+
 if __name__ == "__main__":
-    popup("Hello, World!")
+    # sanity check
+    if not getState().getCurrentProgram():
+        popup("No program selected; unable to emulate.")
+        sys.exit(1)
+    
+    # construct basic ghidra program objects
+    program = getState().getCurrentProgram()
+    ghidra_api = FlatProgramAPI(program)
+
+    # construct core interface class
+    # @TODO determine which program details (e.g. endianess) we need to / should send
+    interface = QilingInterface(program.getExecutablePath(), program.getLanguage().toString())
+
+    # prompt for user input
+    cli_args = askString(f"Executing {program.toString()}", "Command Line Arguments")
+
+    # perform emulation
+    # @TODO update system monitor with progress bar
+    res = interface.emulate(
+        cli_args,
+        lambda prompt: askString("STDIN", prompt),
+        lambda output: popup(output)
+    )
+
+    # handle failure conditions
+    if not res:
+       popup("Emulation failed!")
+       sys.exit(2)
+
+    # format output and visualize
+    annotate_bss(ghidra_api, res.static_variables)
+    annotate_posix_calls(ghidra_api, res.posix_calls)
+    color_function_graph(ghidra_api, res.blocks)
